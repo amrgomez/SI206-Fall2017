b0d96eff74427eea213c365ce487b04596da84e0
Amanda Gomez <amandagomez@amandas-air-2.attlocal.net>
2017-11-26 22:04:03 -0500

    Changes

--- FP/api_info.py
+++ FP/api_info.py
@@ -1,7 +1,11 @@
-#Google Maps API
+#API 1
 consumer_key= ''
 consumer_secret= ''
 access_token=''
 access_token_secret=''
 
-#
+#API 2
+consumer_key2= ''
+consumer_secret2= ''
+access_token2= ''
+access_token_secret2= ''
